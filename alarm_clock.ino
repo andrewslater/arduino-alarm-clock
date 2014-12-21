0fb155087184201c89e3bb15d2569074662176a6
Andrew Slater <andrew@weboniselab.com>
2014-12-21 17:53:56 -0500

    Introduce mode selection

--- alarm_clock.ino
+++ alarm_clock.ino
@@ -6,19 +6,29 @@
 #include "Encoder.h"
 #include "Button.h"
 
-#define ENCODER_THRESHOLD 4
+#define ENCODER_THRESHOLD 4           // Number of encoder events which corresponding to one 'click'
+#define BUTTON_DEBOUNCE_DELAY_MS  25
+
+#define MINUTE_ENCODER_LEFT_PIN   8
+#define MINUTE_ENCODER_RIGHT_PIN  9
+#define HOUR_ENCODER_LEFT_PIN     3
+#define HOUR_ENCODER_RIGHT_PIN    4
+#define MODE_BUTTON_PIN           12
+
+
 
 enum ClockMode {CLOCK, SET_ALARM1, SET_ALARM2, SET_SNOOZE_DURATION};
 
 Adafruit_7segment matrix = Adafruit_7segment();
-Encoder minuteEncoder = Encoder(8,9);
-Encoder hourEncoder = Encoder(3,4);
+Encoder minuteEncoder = Encoder(MINUTE_ENCODER_LEFT_PIN, MINUTE_ENCODER_RIGHT_PIN);
+Encoder hourEncoder = Encoder(HOUR_ENCODER_LEFT_PIN, HOUR_ENCODER_RIGHT_PIN);
+Button modeButton = Button(MODE_BUTTON_PIN, true, true, BUTTON_DEBOUNCE_DELAY_MS);
 ClockMode mode = CLOCK;
 
 void setup() {
 #ifndef __AVR_ATtiny85__
   Serial.begin(9600);
-  Serial.println("Alarm Clock v0.1b");
+  Serial.println("Alarm Clock v0.1c");
 #endif
   matrix.begin(0x70);
 }
@@ -31,6 +41,14 @@ void loop() {
 
   int minuteMovement = minuteEncoder.read() / ENCODER_THRESHOLD;
   int hourMovement = hourEncoder.read() / ENCODER_THRESHOLD;
+
+  modeButton.read();
+  
+  if (modeButton.wasPressed()) {
+    Serial.println("Mode button was pressed");
+    cycleMode();
+    Serial.println(mode, DEC);
+  }
 
   switch(mode) {
     case CLOCK:
@@ -45,6 +63,26 @@ void loop() {
   }
   
 
+}
+
+void cycleMode() {
+  switch(mode) {
+    case CLOCK:
+      mode = SET_ALARM1;
+      break;
+
+    case SET_ALARM1:
+      mode = SET_ALARM2;
+      break;
+
+    case SET_ALARM2:
+      mode = SET_SNOOZE_DURATION;
+      break;
+
+    case SET_SNOOZE_DURATION:
+      mode = CLOCK;
+      break;
+  }
 }
 
 void displayClock(int currentHour, int currentMinute, int currentSecond, int minuteMovement, int hourMovement) {
